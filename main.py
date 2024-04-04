e25cfdc240be964a4a1598be4865a0ee1b2e2ce2
anh210802 <77352818+anh210802@users.noreply.github.com>
2024-04-04 22:54:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
 import sys
 from Adafruit_IO import MQTTClient
 import time
-from serial_com import *
+# from serial_com import *
 
 
 AIO_FEED_IDs = ["cong_tac_den", "cong_tac_quat", "mode_fan", "mode_light", "dat_nhiet_do"]
 AIO_USERNAME = "homeless_da01"
-AIO_KEY = "aio_QRRK86DPc0JmdycIJo3DNtMwf6sS"
+AIO_KEY = "aio_TsSR21eK9a01qylrIVAz2W7P6eY2"
 
 def connected(client):
     print("Ket noi thanh cong ...")
     for topic in AIO_FEED_IDs:
         client.subscribe(topic)
-    writeData("oke_server")
+    # writeData("oke_server")
 
 def subscribe(client , userdata , mid , granted_qos):
     print("Subscribe thanh cong ...")
 
 def disconnected(client):
     print("Ngat ket noi ...")
-    writeData("disconnect_server")
+    # writeData("disconnect_server")
     sys.exit(1)
 
 def message(client , feed_id , payload):
     print("Nhan du lieu: " + payload + " , feed id: " + feed_id)
-    # Cong tac den
-    if feed_id == "cong_tac_den":
-        if payload == "1":
-            writeData("on_led")
-        else:
-            writeData("off_led")
-    # Cong tac quat
-    if feed_id == "cong_tac_quat":
-        if payload == "1":
-            writeData("on_fan")
-        else:
-            writeData("off_fan")
-    # Cong tac den canh bao
-    if feed_id == "mode_light":
-        if payload == "1":
-            writeData("on_led_warn")
-        else:
-            writeData("off_led_warn")
-    # Hen bat quat
-    if feed_id == "mode_fan":
-        if payload == "1":
-            writeData("on_mode_fan")
-            if feed_id == "dat_nhiet_do":
-                writeData(payload)
-        else:
-            writeData("off_mode_fan")
+    # # Cong tac den
+    # if feed_id == "cong_tac_den":
+    #     if payload == "1":
+    #         writeData("on_led")
+    #     else:
+    #         writeData("off_led")
+    # # Cong tac quat
+    # if feed_id == "cong_tac_quat":
+    #     if payload == "1":
+    #         writeData("on_fan")
+    #     else:
+    #         writeData("off_fan")
+    # # Cong tac den canh bao
+    # if feed_id == "mode_light":
+    #     if payload == "1":
+    #         writeData("on_led_warn")
+    #     else:
+    #         writeData("off_led_warn")
+    # # Hen bat quat
+    # if feed_id == "mode_fan":
+    #     if payload == "1":
+    #         writeData("on_mode_fan")
+    #         if feed_id == "dat_nhiet_do":
+    #             writeData(payload)
+    #     else:
+    #         writeData("off_mode_fan")
 
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
